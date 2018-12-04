c55c3b98c88ec202f24b0d24fb1ebf6bbbc1fa82
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-12-04 16:27:30 +0000

    Added preview of uploaded image

--- Instagram/Instagram/wwwroot/js/site.js
+++ Instagram/Instagram/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
 
-// Write your JavaScript code.
+        reader.onload = function (e) {
+            $('#preview')
+                .attr('src', e.target.result);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
